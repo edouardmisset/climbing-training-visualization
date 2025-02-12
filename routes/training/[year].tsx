5531e85ffd400597646967fce26a2c5122207d8b
Edouard Misset <edouardmisset@gmail.com>
2025-02-12 22:18:10 +0100

    feat: update training route to use dynamic API base URL and improve layout

--- routes/training/[year].tsx
+++ routes/training/[year].tsx
@@ -3,12 +3,14 @@ import { Handlers, PageProps } from "$fresh/server.ts"
 import { TrainingSession, trainingSessionSchema } from "types/training.ts"
 import { YearGrid } from "../../components/year-grid.tsx"
 
+const apiBaseUrl = Deno.env.get("env") === "development"
+  ? `http://localhost:8000`
+  : `https://climbing-back.deno.dev/`
+
 export const handler: Handlers<TrainingSession[]> = {
   async GET(_req, ctx) {
     const { data } = await fetch(
-      `https://climbing-back.deno.dev/api/training?year=${
-        ctx.params?.year ?? ""
-      }`,
+      `${apiBaseUrl}/api/training?year=${ctx.params?.year ?? ""}`,
     )
       .then(
         (res) => res.json(),
@@ -21,28 +23,32 @@ export const handler: Handlers<TrainingSession[]> = {
 export default function Visualization(props: PageProps<TrainingSession[]>) {
   const { params: { year }, data } = props
 
-  const sessions = trainingSessionSchema.array().parse(data)
-
   const numberYear = Number(year)
-  const yearSession = sessions.filter((session) =>
-    session.date.year === numberYear
-  )
+  const sessions = trainingSessionSchema.array().parse(data)
 
   const nextYear = numberYear + 1
   const previousYear = numberYear - 1
   return (
     <>
-      <div>An overview of my training in {year}</div>
-      {yearSession.length === 0
-        ? <span>No record</span>
-        : <YearGrid gridContent={yearSession} />}
+      <h1
+        style={{
+          textAlign: "center",
+          margin: "1rem",
+        }}
+      >
+        An overview of my training in {year}
+      </h1>
       <div
         style={{
           display: "flex",
-          justifyContent: "space-between",
+          justifyContent: "space-around",
+          alignItems: "center",
         }}
       >
         <a href={`./${previousYear}`}>{previousYear}</a>
+        {sessions.length === 0
+          ? <span>No record</span>
+          : <YearGrid gridContent={sessions} />}
         <a href={`./${nextYear}`}>{nextYear}</a>
       </div>
     </>
